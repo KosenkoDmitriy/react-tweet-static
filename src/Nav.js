dd67bbe57d5254dca08f3f4eda89583432a51b55
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-02-26 23:41:32 +0300

    throw an error if `NavItem` component isn't found

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Nav({children}) {
+  React.Children.forEach(children, (item) => {
+    item.type === NavItem ? console.log("ok") : console.error("only NavItem allowed") ;
+  })
+
   let items = React.Children.toArray(children);
   for (let i = items.length - 1; i >= 1; i--) {
     items.splice(i, 0,
@@ -17,13 +22,17 @@ function NavItem({url,children}) {
     <a href={url}>{children}</a>
   );
 }
+NavItem.propTypes = {
+  url: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired
+};
 
 function NavMenu() {
   return(
     <Nav>
       <NavItem url='/'>Home</NavItem>
       <NavItem url='/about'>About</NavItem>
-      <NavItem url='/contacts'>Contacts</NavItem>
+      <a href='/contacts'>Contacts</a>
     </Nav>
   );
 }
