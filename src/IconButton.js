68f4804a0e6cc01fa099bd74ed3178ad6865cb16
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-02-26 22:23:13 +0300

    IconButton component with propTypes

--- src/IconButton.js
+++ src/IconButton.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function IconButton({children}) {
   return (
     <button>
-      <i class="fa fa-heart"/>
+      <i className="fa fa-heart"/>
       {children}
     </button>
   );
 }
+
+IconButton.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ])
+};
 
 export default IconButton;
